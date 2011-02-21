e86722ebf5e9268ed0cce767d7659b6534d6314f
Simon Zimmermann <simonz05@gmail.com>
2011-02-21 13:24:25 +0000

    less indent

--- sli.jquery.js
+++ sli.jquery.js
@@ -60,71 +60,73 @@
             });
 
             function animate(direction) {
-                if (!active) {
-                    active = true;
-                    switch(direction) {
-                        case 'next':
-                            // change current slide to previous
-                            prev = current;
-                            // get next from current + 1
-                            next = current + 1;
-                            // if last slide, set next to first slide
-                            next = total === next ? 0 : next;
-                            // set position of next slide to right of previous
-                            position = width * 2;
-                            // distance to slide based on width of slides
-                            direction = -width * 2;
-                            // store new current slide
-                            current = next;
-                            break;
-                        case 'prev':
-                            // change current slide to previous
-                            prev = current;
-                            // get next from current - 1
-                            next = current - 1;
-                            // if first slide, set next to last slide
-                            next = next === -1 ? total - 1 : next;
-                            // set position of next slide to left of previous
-                            position = 0;
-                            // distance to slide based on width of slides
-                            direction = 0;
-                            // store new current slide
-                            current = next;
-                            break;
-                    }
-
-                    // move next slide to right of previous
-                    control.children(':eq('+ next +')').css({
-                        left: position,
-                        display: 'block'
-                    });
-
-                    // animate control
-                    control.animate({
-                        left: direction
-                    }, option.slideSpeed, function() {
-                        // after animation reset control position
-                        control.css({
-                        left: -width
-                        });
-
-                        // reset and show next
-                        control.children(':eq('+ next +')').css({
-                        left: width,
-                        zIndex: 5
-                        });
-
-                        // reset previous slide
-                        control.children(':eq('+ prev +')').css({
-                        left: width,
-                        display: 'none',
-                        zIndex: 0
-                        });
-
-                        // end of animation
-                        active = false;
-                    });
+                if (active) {
+                    return;
                 }
+
+                active = true;
+                switch(direction) {
+                    case 'next':
+                        // change current slide to previous
+                        prev = current;
+                        // get next from current + 1
+                        next = current + 1;
+                        // if last slide, set next to first slide
+                        next = total === next ? 0 : next;
+                        // set position of next slide to right of previous
+                        position = width * 2;
+                        // distance to slide based on width of slides
+                        direction = -width * 2;
+                        // store new current slide
+                        current = next;
+                        break;
+                    case 'prev':
+                        // change current slide to previous
+                        prev = current;
+                        // get next from current - 1
+                        next = current - 1;
+                        // if first slide, set next to last slide
+                        next = next === -1 ? total - 1 : next;
+                        // set position of next slide to left of previous
+                        position = 0;
+                        // distance to slide based on width of slides
+                        direction = 0;
+                        // store new current slide
+                        current = next;
+                        break;
+                }
+
+                // move next slide to right of previous
+                control.children(':eq('+ next +')').css({
+                    left: position,
+                    display: 'block'
+                });
+
+                // animate control
+                control.animate({
+                    left: direction
+                }, option.slideSpeed, function() {
+                    // after animation reset control position
+                    control.css({
+                    left: -width
+                    });
+
+                    // reset and show next
+                    control.children(':eq('+ next +')').css({
+                    left: width,
+                    zIndex: 5
+                    });
+
+                    // reset previous slide
+                    control.children(':eq('+ prev +')').css({
+                    left: width,
+                    display: 'none',
+                    zIndex: 0
+                    });
+
+                    // end of animation
+                    active = false;
+                });
             }; 
         });
     };
